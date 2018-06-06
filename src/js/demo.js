6536410fb72cd778d8a350e89106bc700514d28e
yuewen@hongte.info <yuewen@hongte.info>
2018-06-06 10:01:40 +0800

    撒

---
+++ src/js/demo.js
@@ -0,0 +1,50 @@
+jsoncallback([
+    {
+        "id": 1024,
+        "orgCode": "D0103160101",
+        "orgNameCn": "房贷部",
+        "orgPath": "D01/D0103/D010316/D01031601/D0103160101",
+        "parentOrgCode": "D01031601",
+        "rootOrgCode": "D01"
+    },
+    {
+        "id": 2560,
+        "orgCode": "D010442070701",
+        "orgNameCn": "团队二",
+        "orgPath": "D01/D0104/D010442/D01044207/D0104420707/D010442070701",
+        "parentOrgCode": "D0104420707",
+        "rootOrgCode": "D01"
+    },
+    {
+        "id": 2816,
+        "orgCode": "D0104260901",
+        "orgNameCn": "风控部",
+        "orgPath": "D01/D0104/D010426/D01042609/D0104260901",
+        "parentOrgCode": "D01042609",
+        "rootOrgCode": "D01"
+    },
+    {
+        "id": 2817,
+        "orgCode": "D0104251201",
+        "orgNameCn": "昆明西山营业部",
+        "orgPath": "D01/D0104/D010425/D01042512/D0104251201",
+        "parentOrgCode": "D01042512",
+        "rootOrgCode": "D01"
+    },
+    {
+        "id": 2562,
+        "orgCode": "D010442100401",
+        "orgNameCn": "团队三",
+        "orgPath": "D01/D0104/D010442/D01044210/D0104421004/D010442100401",
+        "parentOrgCode": "D0104421004",
+        "rootOrgCode": "D01"
+    },
+    {
+        "id": 2818,
+        "orgCode": "D0104251401",
+        "orgNameCn": "风控部",
+        "orgPath": "D01/D0104/D010425/D01042514/D0104251401",
+        "parentOrgCode": "D01042514",
+        "rootOrgCode": "D01"
+    }
+])
